c00096f09541d165adc846681fe30cf8d856857b
Youshah <mohdyoushah@gmail.com>
2024-12-03 13:48:24 +0530

    Hello world message for show screen

--- app.ts
+++ app.ts
@@ -17,9 +17,13 @@ mongoose.connection.on("open", () => {
 });
 
 var app = express();
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 app.use(cors()); // enable cors at application level
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
