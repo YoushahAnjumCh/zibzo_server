e14f10c342f8373a09182d18ce22ab85cbe65cba
Youshah <mohdyoushah@gmail.com>
2024-12-03 15:44:36 +0530

    MongoDB database connection

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import path from "path";
+
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -11,13 +11,23 @@ import uploadRouter from "./routes/upload_data.route";
 import cart from "./routes/cart.route";
 
 dotenv.config();
-mongoose.connect(process.env.MONGODB_CONNECTION_STR || "", {});
+const password = process.env.MONGO_PASSWORD;
+
+const connectionString = `mongodb+srv://Youshah4499:${password}@zibzo.tqwnn.mongodb.net/zibzo_server?retryWrites=true&w=majority&appName=ZibZo`;
+
+mongoose.connect(connectionString || "", {});
 mongoose.connection.on("open", () => {
   console.log("Onlineshoppingdb connected successfully !");
 });
 
 var app = express();
 
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+
+  next();
+});
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
--- dist/app.js
+++ dist/app.js
@@ -14,13 +14,23 @@ const order_details_route_1 = __importDefault(require("./routes/order_details.ro
 const upload_data_route_1 = __importDefault(require("./routes/upload_data.route"));
 const cart_route_1 = __importDefault(require("./routes/cart.route"));
 dotenv_1.default.config();
-mongoose_1.default.connect(process.env.MONGODB_CONNECTION_STR || "", {});
+const password = process.env.MONGO_PASSWORD;
+const connectionString = `mongodb+srv://Youshah4499:${password}@zibzo.tqwnn.mongodb.net/?retryWrites=true&w=majority&appName=ZibZo`;
+mongoose_1.default.connect(connectionString || "", {});
 mongoose_1.default.connection.on("open", () => {
     console.log("Onlineshoppingdb connected successfully !");
 });
 var app = (0, express_1.default)();
+app.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
 app.use(express_1.default.json({ limit: "10mb" }));
 app.use(express_1.default.urlencoded({ limit: "10mb", extended: true }));
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 app.use((0, cors_1.default)()); // enable cors at application level
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
