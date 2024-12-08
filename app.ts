a85a094387eec43046ce76ef3d29b62b3b8aad62
Youshah <mohdyoushah@gmail.com>
2024-12-08 11:16:05 +0530

    rerun pipeline

--- app.ts
+++ app.ts
@@ -31,9 +31,6 @@ app.use((_req, res, next) => {
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 app.use(cors()); // enable cors at application level
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
