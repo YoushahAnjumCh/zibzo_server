2059d879e162e50a26509b01ff0e215c8a47f507
Youshah <mohdyoushah@gmail.com>
2024-12-08 11:33:03 +0530

    rerun pipeline

--- dist/app.js
+++ dist/app.js
@@ -15,8 +15,7 @@ const upload_data_route_1 = __importDefault(require("./routes/upload_data.route"
 const cart_route_1 = __importDefault(require("./routes/cart.route"));
 dotenv_1.default.config();
 const password = process.env.MONGO_PASSWORD;
-// const connectionString = `mongodb+srv://Youshah4499:${password}@zibzo.tqwnn.mongodb.net/zibzo_server?retryWrites=true&w=majority&appName=ZibZo`;
-const connectionString = "mongodb://localhost:27017/zibzo_server";
+ const connectionString = `mongodb+srv://Youshah4499:${password}@zibzo.tqwnn.mongodb.net/zibzo_server?retryWrites=true&w=majority&appName=ZibZo`;
 mongoose_1.default.connect(connectionString);
 mongoose_1.default.connection.on("open", () => {
     console.log("Onlineshoppingdb connected successfully !");
